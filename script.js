17c099e8f1658b757a2e1f7f5e16b51cd96512b3
--Explosion-- <explosion.scratch@gmail.com>
2020-10-03 23:21:51 +0000

    It's working!?!

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-$.getJSON('https://scratchdb.lefty.one/v2/user/info/--explosion--', function(data) {
-    
-});
+function get_data(user, attribute) {
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			var myObj = JSON.parse(this.responseText);
+			document.getElementById("test").innerHTML = myObj['followers'];
+		}
+	};
+	xmlhttp.open("GET", "https://scratchdb.lefty.one/v2/user/info/"+ user, true);
+	xmlhttp.send();
+}
