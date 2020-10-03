164aa6c97540fe3ca86501921018bd341a1483aa
--Explosion-- <explosion.scratch@gmail.com>
2020-10-03 23:02:03 +0000

    More code.

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const header = document.querySelector('header');
 const section = document.querySelector('section');
 let requestURL = 'https://scratchdb.lefty.one//v2/user/info/--Explosion--';
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function() {
+  const data = request.response;
+}
