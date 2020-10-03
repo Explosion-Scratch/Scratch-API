742e05d5300942276c25af9c9c738d3125064b06
--Explosion-- <explosion.scratch@gmail.com>
2020-10-03 23:05:15 +0000

    Hmm =/

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const header = document.querySelector('header');
 const section = document.querySelector('section');
-let requestURL = 'https://scratchdb.lefty.one//v2/user/info/--Explosion--';
+let requestURL = 'https://scratchdb.lefty.one/v2/user/info/--Explosion--';
 let request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
